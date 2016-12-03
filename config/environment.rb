d73ae0e0bd59e36a11edb03f06fc7a766e440a1d
Yinka Ash <slankeyinc@gmail.com>
2016-12-03 14:32:16 +0800

    Heroku problem fixed

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,23 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
+
 ActionMailer::Base.smtp_settings = {
-  :address => 'smtp.sendgrid.net',
-  :port => '587',
-  :authentication => :plain,
-  :user_name => ENV['netguru01'],
-  :password => ENV['Success12#'],
+  :user_name => 'apikey',
+  :password => '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',
   :domain => 'heroku.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
   :enable_starttls_auto => true
 }
+
+# ActionMailer::Base.smtp_settings = {
+#   :address => 'smtp.sendgrid.net',
+#   :port => '587',
+#   :authentication => :plain,
+#   :user_name => ENV['SENDGRID_USERNAME'],
+#   :password => ENV['SENDGRID_PASSWOIRRD'],
+#   :domain => 'heroku.com',
+#   :enable_starttls_auto => true
+# }
